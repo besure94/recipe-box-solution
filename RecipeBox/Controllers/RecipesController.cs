1d8014d8b6e306ca43f9e3606587b78aec086ec7
Brian Scherner <brianscherner@gmail.com>
2023-11-29 11:51:18 -0800

    Add View files and Index route/view for Recipes.

---
+++ RecipeBox/Controllers/RecipesController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using RecipeBox.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RecipeBox.Controllers
+{
+  public class RecipesController : Controller
+  {
+    private readonly RecipeBoxContext _db;
+    public RecipesController(RecipeBoxContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Recipe> model = _db.Recipes.ToList();
+      return View(model);
+    }
+
+
+  }
+}
