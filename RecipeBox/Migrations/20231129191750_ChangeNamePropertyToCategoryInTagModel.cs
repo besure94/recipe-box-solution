52a96ba1217ca5999500281f9bd5997643b30f18
Brian Scherner <brianscherner@gmail.com>
2023-11-29 11:22:04 -0800

    Create Database w/ migrations.

---
+++ RecipeBox/Migrations/20231129191750_ChangeNamePropertyToCategoryInTagModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeBox.Migrations
+{
+    public partial class ChangeNamePropertyToCategoryInTagModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Tags",
+                newName: "Category");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "Tags",
+                newName: "Name");
+        }
+    }
+}
