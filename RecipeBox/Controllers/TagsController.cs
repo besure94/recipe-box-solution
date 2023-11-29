4ce3fc4e7e6b19f1e1e91527f2b85b96c3b3c606
Brian Scherner <brianscherner@gmail.com>
2023-11-29 14:02:13 -0800

    Create View files for Tag and update Index view w/ route.

---
+++ RecipeBox/Controllers/TagsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using RecipeBox.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RecipeBox.Controllers
+{
+  public class TagsController : Controller
+  {
+    private readonly RecipeBoxContext _db;
+    public TagsController(RecipeBoxContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Tags.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+  }
+}
