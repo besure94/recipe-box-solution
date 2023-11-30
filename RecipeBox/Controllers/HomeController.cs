065d3b198882ce61f496d721f930d03e02052088
Brian Scherner <brianscherner@gmail.com>
2023-11-30 15:09:54 -0800

    Sort recipes in splash page by highest rating to lowest.

--- RecipeBox/Controllers/HomeController.cs
+++ RecipeBox/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace RecipeBox.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      Recipe[] recipes = _db.Recipes.ToArray();
+      Recipe[] recipes = _db.Recipes.OrderByDescending(r => r.Rating).ToArray();
       Tag[] tags = _db.Tags.ToArray();
       Dictionary<string, object[]> model = new Dictionary<string, object[]>();
       model.Add("recipes", recipes);
